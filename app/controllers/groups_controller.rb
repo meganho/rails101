b996ad85b8c44a4cc80dfd60dd343396e7b7dc99
megan <bbeeaanns@gmail.com>
2016-12-16 20:24:14 +0100

    implement groups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,8 +6,12 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
-  
+
   def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
     @group = Group.find(params[:id])
   end
 
